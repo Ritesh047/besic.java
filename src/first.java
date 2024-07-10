d34efa88732f5a3c78ad6bdfcfb509e1e3146aee
Ritesh047 <riteshkumarbehera43@gmail.com>
2024-07-10 12:10:43 +0530

    age

--- src/first.java
+++ src/first.java
@@ -1,5 +1,10 @@
 public class first {
     public static void main(String[] args) {
-        System.out.println("HELLO RITU");
+        System.out.println("HELLO RIESH");
+        int age = 23;
+        System.out.println("my age "+age);
+
+
+
     }
 }
