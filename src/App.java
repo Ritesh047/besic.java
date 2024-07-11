f9fbf8fbcdedb04f72b96d46004f904c9104a656
Ritesh047 <riteshkumarbehera43@gmail.com>
2024-07-11 12:26:46 +0530

    java

--- src/App.java
+++ src/App.java
@@ -1,13 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, Ritesh!");
-        Car car1 = new Car();
-        int int1 = 10;
-        car1.run();
-
-        Car car2 = new Car();
-        car2.run();
-
+       Team 
 
     }
 }
